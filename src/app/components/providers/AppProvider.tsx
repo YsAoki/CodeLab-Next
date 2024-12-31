31e5c383d7e33a8e27df0ce1d2b89686347cb558
YsAoki <kazuaki.aoki@ysinc.co.jp>
2025-01-01 07:59:35 +0900

    muiの依存関係を全て削除(shadcn/uiに切り替える)

--- src/app/components/providers/AppProvider.tsx
+++ src/app/components/providers/AppProvider.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import { theme } from "@/app/theme";
-import { ThemeProvider } from "@mui/material";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import "dayjs/locale/de";
 import { FC, PropsWithChildren } from "react";
 
@@ -11,9 +7,10 @@ type Props = PropsWithChildren;
 
 const AppProvider: FC<Props> = ({ children }) => {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </LocalizationProvider>
+    // <LocalizationProvider dateAdapter={AdapterDayjs}>
+    //   <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    // </LocalizationProvider>
+    <>{children}</>
   );
 };
 
