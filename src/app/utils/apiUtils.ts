75eaea6ce5bbd4897b0911e95d25cceffafe9a77
YsAoki <kazuaki.aoki@ysinc.co.jp>
2025-01-25 14:04:56 +0900

    fetch先pathの設定誤りにより修正

--- src/app/utils/apiUtils.ts
+++ src/app/utils/apiUtils.ts
@@ -2,12 +2,7 @@ import { httpClient } from "../lib/httpClient";
 
 export const serverFetchData = async <T>(path: string): Promise<T> => {
   try {
-    const baseURL = process.env.API_BASE_URL;
-    if (!baseURL) {
-      throw new Error("API_BASE_URL is not set in the environment variables.");
-    }
-
-    const response = await httpClient.get(`${baseURL}${path}`);
+    const response = await httpClient.get(path);
     return response.data;
   } catch (error) {
     console.error(`Error fetching data from ${path}:`, error);
