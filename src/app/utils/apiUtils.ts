a9fe89487b5ad4953c2e483b2a2c7e5ee03811d4
YsAoki <kazuaki.aoki@ysinc.co.jp>
2025-01-25 22:18:10 +0900

    修正分

--- src/app/utils/apiUtils.ts
+++ src/app/utils/apiUtils.ts
@@ -2,7 +2,7 @@ import { httpClient } from "../lib/httpClient";
 
 export const serverFetchData = async <T>(path: string): Promise<T> => {
   try {
-    const response = await httpClient.get(path);
+    const response = await httpClient.get<T>(path);
     return response.data;
   } catch (error) {
     console.error(`Error fetching data from ${path}:`, error);
