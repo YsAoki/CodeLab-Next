cf12550965822d1c7e8e675ee7d7b39da0d95d57
YsAoki <kazuaki.aoki@ysinc.co.jp>
2025-01-26 14:30:04 +0900

    修正修正

--- src/app/utils/apiUtils.ts
+++ src/app/utils/apiUtils.ts
@@ -1,8 +1,12 @@
-import { httpClient } from "../lib/httpClient";
+import axios from "axios";
 
 export const serverFetchData = async <T>(path: string): Promise<T> => {
+  const baseURL = process.env.API_BASE_URL; // 環境変数からベースURLを取得
+  if (!baseURL) throw new Error("API_BASE_URL is not defined.");
+
   try {
-    const response = await httpClient.get<T>(path);
+    const fullURL = `${baseURL}${path}`;
+    const response = await axios.get<T>(fullURL);
     return response.data;
   } catch (error) {
     console.error(`Error fetching data from ${path}:`, error);
